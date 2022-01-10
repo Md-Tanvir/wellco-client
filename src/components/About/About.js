fe3e703be1589e20cdd1d23222221c4b8e0e64dc
Md-Tanvir <contact.mdtanvirahmed@gmail.com>
2022-01-11 00:43:28 +0600

    added about area and applied animation

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,50 @@
-import React from 'react';
-
+import React from "react";
+import about from "../../image/about.png";
+import "./About.css";
+import img1 from "../../image/scholarship.png";
+import img2 from "../../image/cap.png";
 const About = () => {
-    return (
-        <div>
-            <h2>About</h2>
+  return (
+    <div className="container py-5">
+      <div className="row gy-5">
+        <div className="col-12 col-md-12 col-lg-6 text-center" data-aos="fade-right">
+          <img src={about} className="img-fluid about-img" alt="" />
         </div>
-    );
+        <div className="col-12 col-md-12 col-lg-6 about-text">
+          <h5 data-aos="fade-left"> WHAT WE PROVIDE</h5>
+          <h2 data-aos="fade-left">We Care About Your Life For Better</h2>
+          <p data-aos="fade-left">
+            Forging relationships between multi to national corporations,
+            governments and global NGOs begins.
+          </p>
+          <div className="row p-4 ps-0" data-aos="fade-up">
+            <div className="col-2 col-md-1">
+              <img src={img1} className="about-icon" alt="" />
+            </div>
+            <div className="col-10 col-md-7">
+              <h5>Master Certified Coach</h5>
+              <p>
+                Our purpose is to build solutions that remove barriers
+                preventing people.
+              </p>
+            </div>
+          </div>
+          <div className="row p-4 ps-0" data-aos="fade-up">
+            <div className="col-2 col-md-1">
+              <img className="about-icon" src={img2} alt="" />
+            </div>
+            <div className="col-10 col-md-7">
+              <h5>Coach Certification Program</h5>
+              <p>
+                Learning and Growth. We’re building a learning organization, so
+                you not only develop.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Home/HeroArea/HeroArea.js
+++ src/components/Home/HeroArea/HeroArea.js
@@ -6,14 +6,14 @@ const HeroArea = () => {
     <div className='hero-area'>
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-12 col-md-12 col-lg-6 hero-text " data-aos="fade-right">
-            <h1>People Expect to be Bored eLearning.</h1>
-            <p>
+          <div className="col-12 col-md-12 col-lg-6 hero-text " >
+            <h1 data-aos="fade-right">People Expect to be Bored eLearning.</h1>
+            <p data-aos="fade-right">
               Every act of conscious learning requires the willingness to suffer
               an injury to one’s self-esteem.
             </p>
-            <button className="hero-btn me-3 mb-4">GET STARTED</button>
-            <button className="hero-btn2">DISCOVER MORE</button>
+            <button data-aos="fade-up" className="hero-btn me-3 mb-4">GET STARTED</button>
+            <button data-aos="fade-up" className="hero-btn2">DISCOVER MORE</button>
           </div>
           <div className="col-12 col-md-12 col-lg-6" data-aos="fade-left">
               <img src={banner} className='img-fluid' alt="" />
