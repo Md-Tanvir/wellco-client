841f4e6cf588f8c7fcca85a8609ac29f12c9ae77
Md-Tanvir <contact.mdtanvirahmed@gmail.com>
2022-01-10 23:47:39 +0600

    AOS package added and applied

--- src/components/Home/HeroArea/HeroArea.js
+++ src/components/Home/HeroArea/HeroArea.js
@@ -6,7 +6,7 @@ const HeroArea = () => {
     <div className='hero-area'>
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-12 col-md-12 col-lg-6 hero-text">
+          <div className="col-12 col-md-12 col-lg-6 hero-text " data-aos="fade-right">
             <h1>People Expect to be Bored eLearning.</h1>
             <p>
               Every act of conscious learning requires the willingness to suffer
@@ -15,7 +15,7 @@ const HeroArea = () => {
             <button className="hero-btn me-3 mb-4">GET STARTED</button>
             <button className="hero-btn2">DISCOVER MORE</button>
           </div>
-          <div className="col-12 col-md-12 col-lg-6">
+          <div className="col-12 col-md-12 col-lg-6" data-aos="fade-left">
               <img src={banner} className='img-fluid' alt="" />
           </div>
         </div>
