322ed3fc45143092b3fbd3b612fe3a98695555a1
Md-Tanvir <contact.mdtanvirahmed@gmail.com>
2022-01-12 02:56:41 +0600

    initial dashboard setup done

--- src/components/Dashboard/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard/Dashboard.js
@@ -1,6 +1,14 @@
 import React from "react";
-import { NavLink, useRouteMatch } from "react-router-dom";
+import { NavLink, Route, Switch, useRouteMatch } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
+import AddCourse from "../AddCourse/AddCourse";
+import DashboardHome from "../DashboardHome/DashboardHome";
+import MakeAdmin from "../MakeAdmin/MakeAdmin";
+import ManageCourses from "../ManageCourses/ManageCourses";
+import ManageOrders from "../ManageOrders/ManageOrders";
+import MyOrders from "../MyOrders/MyOrders";
+import PublishBlog from "../PublishBlog/PublishBlog";
+import Review from "../Review/Review";
 import "./Dashboard.css";
 
 const Dashboard = () => {
@@ -41,10 +49,13 @@ const Dashboard = () => {
             </div>
             <div className="offcanvas-body">
               <div className="dash-nav">
-                {admin && (
+                {/* {admin && ( */}
                   <>
                     <NavLink to={`${url}/manageAllOrders`}>
                       <h5>Manage All Orders</h5>
+                    </NavLink>
+                    <NavLink to={`${url}/publishBlog`}>
+                      <h5>Publish Blog</h5>
                     </NavLink>
                     <NavLink to={`${url}/manageCourses`}>
                       <h5>Manage Courses</h5>
@@ -55,19 +66,20 @@ const Dashboard = () => {
                     <NavLink to={`${url}/makeAdmin`}>
                       <h5>Make Admin</h5>
                     </NavLink>
+                    
                   </>
-                )}
+                {/* )} */}
 
-                {!admin && (
+                {/* {!admin && ( */}
                   <>
                     <NavLink to={`${url}/myOrder`}>
-                      <h5>My Order</h5>
+                      <h5>My Orders</h5>
                     </NavLink>
                     <NavLink to={`${url}/review`}>
                       <h5>Give Review</h5>
                     </NavLink>
                   </>
-                )}
+                {/* )} */}
                 <div className="text-center">
                   <button onClick={logout} className="btn btn-danger mt-3">
                     Log Out
@@ -76,6 +88,35 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
+        </div>
+        <div className="dashboard-area">
+          <Switch>
+            <Route exact path={path}>
+              <DashboardHome></DashboardHome>
+            </Route>
+
+            <Route path={`${path}/makeAdmin`}>
+              <MakeAdmin></MakeAdmin>
+            </Route>
+            <Route path={`${path}/manageCourses`}>
+              <ManageCourses></ManageCourses>
+            </Route>
+            <Route path={`${path}/addCourse`}>
+              <AddCourse></AddCourse>
+            </Route>
+            <Route path={`${path}/review`}>
+              <Review></Review>
+            </Route>
+            <Route path={`${path}/myOrder`}>
+              <MyOrders></MyOrders>
+            </Route>
+            <Route path={`${path}/manageOrders`}>
+              <ManageOrders></ManageOrders>
+            </Route>
+            <Route path={`${path}/publishBlog`}>
+             <PublishBlog></PublishBlog>
+            </Route>
+          </Switch>
         </div>
 
       </div>
